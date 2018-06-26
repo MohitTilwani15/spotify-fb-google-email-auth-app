e3541bb5aa87f8c20e35e75ad39ab9b8bb0a577b
Mohit Tilwani <mtilwani@deloitte.de>
2018-06-26 15:37:13 +0200

    add graphql and relay and node dependencies

---
+++ client/src/createRelay.js
@@ -0,0 +1,43 @@
+import { Environment, Network, RecordSource, Store } from 'relay-runtime';
+const { installRelayDevTools } = require('relay-devtools');
+
+installRelayDevTools();
+
+export default function createRelay() {
+  function fetchQuery(operation, variables) {
+    if (typeof window.data !== 'undefined') {
+      const data = window.data;
+      delete window.data;
+      return Promise.resolve(data);
+    }
+
+    return fetch('/graphql', {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json',
+      },
+      body: JSON.stringify({
+        query: operation.text,
+        variables,
+      }),
+    })
+      .then(res => res.json())
+      .then(payload => {
+        const error = (payload.errors || []).find(x =>
+          [401, 403].includes(x.code),
+        );
+
+        if (error) {
+          throw error;
+        }
+
+        return payload;
+      });
+  }
+
+  const recordSource = new RecordSource();
+  const store = new Store(recordSource);
+  const network = Network.create(fetchQuery);
+
+  return new Environment({ store, network });
+}
